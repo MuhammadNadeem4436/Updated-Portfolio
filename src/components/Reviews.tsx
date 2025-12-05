3387d2a78d80d4804c67a86fcd2f14dd35ebcefb
Muhammad Nadeem Akram <110301976+MuhammadNadeem4436@users.noreply.github.com>
2025-12-05 12:48:02 +0500

    Updated Reviews.tsx

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -15,33 +15,6 @@ const Reviews: React.FC = () => {
   const reviews: Review[] = [
     {
       id: 1,
-      name: 'John Smith',
-      position: 'CTO',
-      company: 'TechStart',
-      avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      rating: 5,
-      content: 'Nadeem delivered our web application ahead of schedule and with exceptional quality. His technical skills and attention to detail are impressive.'
-    },
-    {
-      id: 2,
-      name: 'Sarah Johnson',
-      position: 'Product Manager',
-      company: 'InnovateCorp',
-      avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      rating: 5,
-      content: 'Working with Nadeem was a great experience. He understood our requirements perfectly and created a solution that exceeded our expectations.'
-    },
-    {
-      id: 3,
-      name: 'Michael Lee',
-      position: 'Founder',
-      company: 'StartupX',
-      avatar: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      rating: 4,
-      content: 'Nadeem helped us transform our outdated system into a modern, efficient platform. His technical knowledge and problem-solving skills were invaluable.'
-    },
-    {
-      id: 4,
       name: 'D. Griffin',
       position: 'Client',
       company: 'Fiverr',
@@ -50,7 +23,7 @@ const Reviews: React.FC = () => {
       content: 'My freelancer was very polite and knowledgeable. I appreciated his follow through and attention to detail. This was my first time using Fiverr, and I\'m glad I got the level of work completed for our project. Thank you.'
     },
     {
-      id: 5,
+      id: 2,
       name: 'M. Best',
       position: 'Client',
       company: 'Fiverr',
@@ -59,7 +32,7 @@ const Reviews: React.FC = () => {
       content: 'Nadeem has done an excellent job! He really understands the project and took the time to gather all information needed. He was able to see features that I didn\'t realize I needed. I intend to work with Nadeem on future software development.'
     },
     {
-      id: 6,
+      id: 3,
       name: 'Kayla Sping',
       position: 'Client',
       company: 'Fiverr',
